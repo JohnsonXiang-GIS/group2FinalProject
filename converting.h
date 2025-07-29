166263419a369f81ebbdc0b80c2941ac8fb7f2d4
Nana-Yaw-Dev <gasuoliver@gmail.com>
2025-07-28 23:04:57 -0400

    Add files via upload

---
+++ converting.h
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void convecting(void);
+#endif 
---
+++ convertingmain.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void) {
+	converting();
+
+	return 0;
+}

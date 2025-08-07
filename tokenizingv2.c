d56ba5a210c885be9422a8e19893db7913b96008
Osman-H <hosman14@myseneca.ca>
2025-08-06 23:06:00 -0400

    Update tokenizingv2.c

--- tokenizingv2.c
+++ tokenizingv2.c
@@ -26,22 +26,23 @@ void tokenizing(void) {
 	printf("*** End of Tokenizing Words Demo ***\n\n");*/
 
 	//V2
-	printf("*** Start of Tokenizing Phrases Demo ***\n");
-	char    phrases[BUFFER_SIZE];
-	char*   nextPhrase = NULL;
-	int     phrasesCounter;
+	printf("*** Start of Tokenizing Phrases Demo ***\n"); // Start Message to Indicate The Demo Type
+	char    phrases[BUFFER_SIZE]; // Declare char variable "Phrases" with Buffer Size
+	char*   nextPhrase = NULL; // Declare char Variable "nextPhrase" with NULL value
+	int     phrasesCounter; // Declare int variable "phrasesCounter" without any value yet
 	do {
-		printf("Type a few phrases seperated by comma (q - to quit):\n");
-		fgets(phrases, BUFFER_SIZE, stdin);
-		phrases[strlen(phrases) - 1] = '\0';
-		if ((strcmp(phrases, "q") != 0)) {
-			nextPhrase = strtok(phrases, ",");
-			phrasesCounter = 1;
+		printf("Type a few phrases seperated by comma (q - to quit):\n"); // Print statement asking user to type a phrase seperated by a comma
+		fgets(phrases, BUFFER_SIZE, stdin); // Asks for user input with newline character
+		phrases[strlen(phrases) - 1] = '\0'; // Check for length of phrase and add a null terminator
+		if ((strcmp(phrases, "q") != 0)) { // If the phrase is not "q" (character to quit the program), seperate words through comma
+			nextPhrase = strtok(phrases, ","); // strtok tokenizes the inputted phrase that has a comma
+			phrasesCounter = 1; 
 			while (nextPhrase) {
-				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase); // Displays tokenized phrases
 				nextPhrase = strtok(NULL, ",");
 			}
 		}
-	} while (strcmp(phrases, "q") != 0);
-	printf("*** End of Tokenizing Phrases Demo ***\n\n");
+	} while (strcmp(phrases, "q") != 0); // Continue until user inputs "q"
+	printf("*** End of Tokenizing Phrases Demo ***\n\n"); // End Message 
+
 }
